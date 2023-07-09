e3310f259db9efd91dda5503738667f659dfb607
tsunacan <sin2016iba@gmail.com>
2023-07-09 17:36:33 +0900

    use react-hook-form

--- src/screens/homeScreen/homeScreen.tsx
+++ src/screens/homeScreen/homeScreen.tsx
@@ -1,13 +1,30 @@
 import React, {useState} from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, TextInput} from 'react-native';
 import Button from '../../components/button/button';
 import styles from './homeScreen.style';
 import Input from '../../components/input/input';
 import {SCREENS} from '../../utils/constants';
 import DropDownPicker from 'react-native-dropdown-picker';
 import {zeroPaddiong, calcMaxValue} from '../../utils/padding';
+import {useForm, Controller, set} from 'react-hook-form';
+
+export type FormData = {
+  formFixedValue: string;
+  formVariableValue: string;
+};
 
 const HomeScreen = ({navigation}) => {
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<FormData>({
+    mode: 'onChange',
+    defaultValues: {
+      formFixedValue: '',
+      formVariableValue: '',
+    },
+  });
   const [fixedValue, setFixedValue] = useState('');
   const [variableValue, setVariableValue] = useState('');
   const [open, setOpen] = useState(false);
@@ -24,21 +41,61 @@ const HomeScreen = ({navigation}) => {
     {label: '8', value: 9},
   ]);
 
+  const onSubmit = (data: FormData) => {
+    navigation.navigate(SCREENS.QR_CODE, {
+      fixedValue: data.formFixedValue,
+      variableValue: zeroPaddiong(data.formVariableValue, digits),
+      digits: digits,
+    });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Input
-        label="Fixed value"
-        onChangeText={setFixedValue}
-        placeholder="Enter text here"
-        errorMessage="Please input number"
-      />
-      <Input
-        label="Variable value"
-        onChangeText={setVariableValue}
-        placeholder="Enter text here"
-        errorMessage="Please input number"
-        keyboradType="numeric"
-      />
+      <View>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <TextInput
+              placeholder="Fixed value"
+              onBlur={onBlur}
+              onChangeText={value => {
+                onChange(value);
+                setFixedValue(value);
+              }}
+              value={value}
+            />
+          )}
+          name="formFixedValue"
+        />
+        {errors.formFixedValue && <Text>This is required.</Text>}
+
+        <Controller
+          control={control}
+          rules={{
+            maxLength: 10,
+            pattern: /^[0-9]*$/,
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <TextInput
+              placeholder="Variable value"
+              onBlur={onBlur}
+              onChangeText={value => {
+                onChange(value);
+                setVariableValue(value);
+              }}
+              value={value}
+              keyboardType="numeric"
+            />
+          )}
+          name="formVariableValue"
+        />
+        {errors.formVariableValue && (
+          <Text>Please input only numeric value.</Text>
+        )}
+      </View>
       <DropDownPicker
         open={open}
         value={digits}
@@ -54,13 +111,7 @@ const HomeScreen = ({navigation}) => {
       <Button
         text="Start Generate"
         disabled={fixedValue === '' && variableValue === ''}
-        onClick={() => {
-          navigation.navigate(SCREENS.QR_CODE, {
-            fixedValue: fixedValue,
-            variableValue: zeroPaddiong(variableValue, digits),
-            digits: digits,
-          });
-        }}
+        onClick={handleSubmit(onSubmit)}
       />
     </SafeAreaView>
   );
