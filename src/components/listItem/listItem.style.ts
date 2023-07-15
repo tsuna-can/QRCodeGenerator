dbcc40ae074c62c47950eb846f63707de3657c6e
tsunacan <sin2016iba@gmail.com>
2023-07-16 00:14:51 +0900

    update listItem component

---
+++ src/components/listItem/listItem.style.ts
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flex: 11,
+  },
+  titleWrapper: {
+    flexDirection: 'row',
+  },
+  title: {
+    flex: 1,
+  },
+  iconContainer: {
+    flex: 1,
+  },
+});
+
+export default styles;
--- src/components/listItem/listItem.tsx
+++ src/components/listItem/listItem.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import {ListItem as RNEListItem} from '@rneui/themed';
+import {View, TouchableOpacity} from 'react-native';
+import {ListItem as RNEListItem, Divider} from '@rneui/themed';
 import COLORS from '../../theme/colors';
+import {Icon} from '@rneui/base';
+import styles from './listItem.style';
 
 type listItemProps = {
   title: string;
@@ -15,16 +18,17 @@ const ListItem = ({
 }: listItemProps) => {
   return (
     <>
-      <RNEListItem onPress={onPress}>
-        <RNEListItem.Content>
-          <RNEListItem.Title>{title}</RNEListItem.Title>
+      <RNEListItem bottomDivider={true}>
+        <RNEListItem.Content style={styles.titleContainer}>
+          <TouchableOpacity onPress={onPress} style={styles.titleWrapper}>
+            <RNEListItem.Title style={styles.title}>{title}</RNEListItem.Title>
+          </TouchableOpacity>
         </RNEListItem.Content>
-        <RNEListItem.Chevron
-          onPress={onPressDelete}
-          color={COLORS.RED}
-          type="antdesign"
-          name="delete"
-        />
+        <RNEListItem.Content right={true} style={styles.iconContainer}>
+          <TouchableOpacity onPress={onPressDelete}>
+            <Icon name="delete" type="antdesign" color={COLORS.RED} />
+          </TouchableOpacity>
+        </RNEListItem.Content>
       </RNEListItem>
     </>
   );
